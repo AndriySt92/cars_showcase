7b1b3d781393d7e864d0baf0650ee8b0062b560e
AndriySt92 <andriyua-1992@mail.ru>
2024-03-26 20:11:51 +0200

    Changed header classes

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,7 +4,7 @@ import { navLinks } from "../constants";
 
 const Nav = () => {
   return (
-    <header className="padding-x py-8 absolute z-10 w-full">
+    <header className="padding-x pt-8 py-8 fixed z-50 w-full bg-white bg-opacity-70 backdrop-blur-sm">
       <nav className="flex justify-between items-center max-container">
         <a href="/">
           <img
